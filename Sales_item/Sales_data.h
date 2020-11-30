10c2f5c54f221be2e6762d2f34cc0b8a7e87fa85
Marina <mvtravell@mail.ru>
2020-11-30 05:32:38 +0300

    Sales item

---
+++ Sales_item/Sales_data.h
@@ -0,0 +1,21 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+
+
+
+
+
+
+#endif
