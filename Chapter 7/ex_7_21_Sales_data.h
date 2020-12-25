96cb5921b39e8d93901b780fdd16ba656c152b23
Marina-Ananyeva <75132243+Marina-Ananyeva@users.noreply.github.com>
2020-12-25 22:37:35 +0300

    Update ex_7_21_Sales_data.h

--- Chapter 7/ex_7_21_Sales_data.h
+++ Chapter 7/ex_7_21_Sales_data.h
@@ -11,18 +11,18 @@ class Sales_data
   friend std::istream &read(std::istream &, Sales_data &);
 
   public:
-  Sales_data() = default;
-  Sales_data(const std::string &s) : bookNo(s) {}
-  Sales_data(const std::string &s, unsigned n, double p) : bookNo(), units_sold(), revenue(p*n) {}
-  Sales_data(std::istream &);
-  std::string isbn() const { return bookNo; }
-  Sales_data &combine(const Sales_data &);
+   Sales_data() = default;
+   Sa les_data(const std::string &s) : bookNo(s) {}
+    Sales_data(const std::string &s, unsigned n, double p) : bookNo(), units_sold(), revenue(p*n) {}
+    Sales_da (std::istream &);
+    std::string isbn() const { return bookNo; }
+    Sales_data &combine(const Sales_data &);
   
   private:
-  double avg_price() const;
-  std::string bookNo;
-  unsigned units_sold = 0;
-  double revenue = 0.0;
+    double avg_price() const;
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
 };
 
 Sales_data add(const Sales_data &, const Sales_data &);
